8c57009a4ddc9c7b777f9553ca80d2bc3433316c
Dameris <116887141+Dameris@users.noreply.github.com>
2023-03-02 16:28:14 +0100

    Función Daniel

--- src/main/kotlin/com/hundirflota/clases/Tablero.kt
+++ src/main/kotlin/com/hundirflota/clases/Tablero.kt
@@ -1,4 +1,19 @@
 package com.hundirflota.clases
 
 class Tablero {
+
+        private fun colocarLancha(lancha: Barco) {
+        val random = Random()
+        var x = 0
+        var y = 0
+        var colocado = false
+        while (!colocado) {
+            x = random.nextInt(9)
+            y = random.nextInt(9)
+            if (comprobarSiLibre(x, y)) {
+                this.tablero.get(x).get(y) = lancha.getIni()
+                colocado = true
+            }
+        }
+    }
 }
