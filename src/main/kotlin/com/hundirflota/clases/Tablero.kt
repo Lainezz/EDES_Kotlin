9e5adf3e0490ec40e19c5d1b68eeda0b4abfe737
Alejandro <adiabar0510@g.educaand.es>
2023-03-02 16:35:29 +0100

    He realizado la creacion de variables y el constructor

--- src/main/kotlin/com/hundirflota/clases/Tablero.kt
+++ src/main/kotlin/com/hundirflota/clases/Tablero.kt
@@ -1,4 +1,16 @@
 package com.hundirflota.clases
 
 class Tablero {
+    private var NFILAS = 10
+    private var NCOLS = 10
+    private var guion:Char =45
+    private var arroba:Char = 64
+    private var posicionesBarcos:Int
+
+    fun Tablero() {
+        this.tablero = Array<CharArray>(NFILAS) { CharArray(NCOLS) }
+        setPosicionesBarcos(0)
+        llenarTableroVacio()
+    }
+
 }
