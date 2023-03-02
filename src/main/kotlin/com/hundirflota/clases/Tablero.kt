3e64088e100b736d0a296ba0d4855216b789ca59
Esther <eniemed@g.educaand.es>
2023-03-02 16:40:02 +0100

    Implementación de ula función CheckDisparo que comprueba si el disparo realizado da en el blanco o en el agua

--- src/main/kotlin/com/hundirflota/clases/Tablero.kt
+++ src/main/kotlin/com/hundirflota/clases/Tablero.kt
@@ -1,4 +1,15 @@
 package com.hundirflota.clases
 
 class Tablero {
+
+    fun checkDisparo(x: Int, y: Int): Char {
+        if (this.tablero[x][y] != guion) {
+            println("Tocado")
+            setPosicionesBarcos(getPosicionesBarcos() - 1)
+            return this.tablero[x][y]
+        } else {
+            println("Agua")
+            return arroba
+        }
+    }
 }
