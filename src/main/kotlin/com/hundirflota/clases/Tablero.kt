3851e73c47dc7d7899114fe66a4fa9c025c2accf
Diego Gutierrez <dieguga04@gmail.com>
2023-03-02 16:40:02 +0100

    Implementacion de la funcion rellenar tablero

--- src/main/kotlin/com/hundirflota/clases/Tablero.kt
+++ src/main/kotlin/com/hundirflota/clases/Tablero.kt
@@ -1,4 +1,21 @@
 package com.hundirflota.clases
 
 class Tablero {
+
+ fun llenarTableroVacio(){
+
+    for (i in 0 .. tablero.size){
+         for ( j in 0 .. this.tablero[i].length) {
+         this.tablero[i][j] = guion }
+
+ }
+
+
+
+
+
+
+
+
+
 }
