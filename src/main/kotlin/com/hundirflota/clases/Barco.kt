b47c4d4ad4fbc7770f36c57e30b150326aab4839
Fran <Fran_Diz@outlook.com>
2023-03-02 16:29:07 +0100

    colocarBarco OK

--- src/main/kotlin/com/hundirflota/clases/Barco.kt
+++ src/main/kotlin/com/hundirflota/clases/Barco.kt
@@ -1,4 +1,13 @@
 package com.hundirflota.clases
 
 class Barco(nombre: String, ini: Char, tamanio: Int) {
+
+    private fun colocarBarco(barco : Barco){
+        when(barco.ini){
+            "L"->colocarL(Barco)
+            "B","Z"->colocarHorizontal(Barco)
+            "P"->colocarVertical(barco)
+            else-> imprimirError("Barco erróneo")
+        }
+    }
 }
