16279fb6074b5e27cca67e937281a686c678369a
Lainezz <diegolinaresmojacar@gmail.com>
2023-03-09 15:01:01 +0100

    arreglando repo

--- src/main/kotlin/com/hundirflota/clases/Barco.kt
+++ src/main/kotlin/com/hundirflota/clases/Barco.kt
@@ -2,54 +2,5 @@ package com.hundirflota.clases
 
 class Barco(nombre: String, ini: Char, tamanio: Int) {
 
-    fun getIni(): Char {
-        return ini
-    }
 
-    @kotlin.Throws(IllegalArgumentException::class)
-    fun setIni(ini: Char) {
-        for (c in listaAdmitidos) {
-            if (c == ini) {
-                ini = ini
-                break
-            }
-        }
-        if (ini !== ini) throw IllegalArgumentException("Inicial no admitida")
-
-        //Si se ha establecido la inicial correctamente, establecemos el nombre y el tamanio del Barco
-        setNombre(ini)
-        setTamanio(ini)
-    }
-
-    fun getTamanio(): Int {
-        return tamanio
-    }
-
-    private fun setTamanio(ini: Char) {
-        when (ini) {
-            'L' -> {
-                this.tamanio = 1
-            }
-
-            'B' -> {
-                this.tamanio = 3
-            }
-
-            'Z' -> {
-                this.tamanio = 4
-            }
-
-            'P' -> {
-                this.tamanio = 5
-            }
-
-            else -> throw IllegalArgumentException("Unexpected value: $ini")
-    private fun colocarBarco(barco : Barco){
-        when(barco.ini){
-            "L"->colocarL(barco)
-            "B","Z"->colocarHorizontal(barco)
-            "P"->colocarVertical(barco)
-            else-> imprimirError("Barco erróneo")
-        }
-    }
 }
