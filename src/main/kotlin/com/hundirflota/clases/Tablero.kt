e322c79ea75afc498e25fdb8014239300eaefc9c
Patricio <pcifrey501@g.educaand.es>
2023-03-09 16:21:30 +0100

    creación método mostrarTablero

--- src/main/kotlin/com/hundirflota/clases/Tablero.kt
+++ src/main/kotlin/com/hundirflota/clases/Tablero.kt
@@ -221,6 +221,33 @@ class Tablero {
         System.out.flush();
     }*/
 
+    open fun mostrarTablero() {
+
+        // Imprimo dos espacios
+        print("  ")
+
+        // La letra A en decimal es el numero 65
+        var letra = 65
+        for (i in 0 until NCOLS) {
+            System.out.format("%2c", letra.toChar())
+            letra++
+        }
+        // Imprimo un salto de linea
+        println()
+
+        // Procedo a mostrar el contenido de mi tablero
+        for (i in 0 until tablero.length) {
+            // Primero imprimo el numero de linea
+            System.out.format("%2d", i + 1)
+            for (j in 0 until tablero.get(i).length) {
+                System.out.format("%2c", this.tablero.get(i).get(j))
+            }
+            // Al terminar la fila, imprimo un salto de línea
+            println()
+        }
+        System.out.flush()
+    }
+
 
 
     /*
